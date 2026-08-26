6a3c800fb3125b9c6c07558ac83b147599b76fa4
Don_Ferz <Don_Ferz@mail.ru>
2026-08-26 15:50:12 +0300

    Создание схем user, salon, master, service в папке schemas

--- Clicuster_beta/clicuster_project/app/schemas/master.py
+++ Clicuster_beta/clicuster_project/app/schemas/master.py
@@ -1,29 +1,31 @@
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
-from app.models.enums import UserRole
+from app.models.enums import UserRole, MasterPosition
 from typing import Optional
 from pydantic import ConfigDict
 
 
-class UserBase(BaseModel):
+class MasterBase(BaseModel):
     full_name: str
-    role: UserRole = UserRole.CLIENT
+    role: UserRole = UserRole.MASTER
+    master_position: MasterPosition
     email: EmailStr
     phone: Optional[str] = None
 
 
-class UserCreate(UserBase):
+class MasterCreate(MasterBase):
     password: str
 
 
-class UserUpdate(BaseModel):
+class MasterUpdate(BaseModel):
     full_name: Optional[str] = None
+    master_position: Optional[MasterPosition] = None
     email: Optional[EmailStr] = None
     phone: Optional[str] = None
     is_active: Optional[bool] = None
 
 
-class UserRead(UserBase):
+class MasterRead(MasterBase):
     id: int
     is_active: bool
     created_at: datetime
